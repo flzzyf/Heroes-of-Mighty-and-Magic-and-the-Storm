43659a23cc9045519243607e2c04d6eabcc8e64e
flzzyf <2981019122@qq.com>
2018-09-07 21:08:35 +0800

    主界面

---
+++ Heroes of Mighty and Magic and the Storm/Assets/Scripts/Button_Special.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button_Special : Button
+{
+    public AudioClip clip;
+
+    public int a;
+
+    protected override void Start()
+    {
+
+    }
+
+}
---
+++ Heroes of Mighty and Magic and the Storm/Assets/Scripts/Button_SpecialEditor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(Button_Special))]
+public class Button_SpecialEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Button_Special button = (Button_Special)target;
+
+        base.OnInspectorGUI();
+    }
+}
