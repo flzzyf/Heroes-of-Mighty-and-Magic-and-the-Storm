18fb802c28106f7ed9cb9b552d106abc1629c0eb
flzzyf <2981019122@qq.com>
2018-10-18 16:11:31 +0800

    改善单位类型UI，移除数组插件

---
+++ Heroes of Mighty and Magic and the Storm/Assets/Editor/UnitTypeEditor.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(UnitType))]
+public class UnitTypeEditor : Editor
+{
+    string[] tab_main = { "level", "att", "def", "damage", "hp",
+        "speed", "growth", "AIValue", "cost", "traits", "attackType",
+        "size", "armorType", "icon", "race", "animControl", };
+
+    string[] tab_sound = { "sound_attack", "sound_walk", "sound_hit", "sound_death" };
+
+    public override void OnInspectorGUI()
+    {
+        UnitType type = (UnitType)target;
+
+        type.tab = GUILayout.Toolbar(type.tab, new string[] { "Main", "Sound" });
+        if (type.tab == 0)
+        {
+            for (int i = 0; i < tab_main.Length; i++)
+            {
+                SerializedProperty property = serializedObject.FindProperty(tab_main[i]);
+                if (property.isArray)
+                {
+                    zyf.ShowList(property);
+                }
+                else
+                {
+                    EditorGUILayout.PropertyField(property);
+                }
+
+                //远程攻击特殊选项
+                if (tab_main[i] == "attackType")
+                {
+                    if (type.attackType == AttackType.range)
+                    {
+                        EditorGUI.indentLevel += 1;
+                        EditorGUILayout.PropertyField(serializedObject.FindProperty("ammo"));
+                        EditorGUILayout.PropertyField(serializedObject.FindProperty("missile"));
+                        EditorGUILayout.PropertyField(serializedObject.FindProperty("launchPos"));
+                        EditorGUI.indentLevel -= 1;
+                    }
+                }
+            }
+        }
+        else if (type.tab == 1)
+        {
+            for (int i = 0; i < tab_sound.Length; i++)
+            {
+                SerializedProperty property = serializedObject.FindProperty(tab_sound[i]);
+                if (property.isArray)
+                {
+                    zyf.ShowList(property);
+                }
+                else if (property.type == typeof(FixedSound).Name)
+                {
+                    EditorGUILayout.PropertyField(property);
+                    if (property.isExpanded)
+                    {
+                        EditorGUI.indentLevel += 1;
+                        EditorGUILayout.PropertyField(property.FindPropertyRelative("clip"), GUIContent.none);
+                        EditorGUILayout.PropertyField(property.FindPropertyRelative("skipDuration"));
+                        EditorGUI.indentLevel -= 1;
+                    }
+                }
+                else
+                    EditorGUILayout.PropertyField(property);
+            }
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
--- Heroes of Mighty and Magic and the Storm/Assets/ScriptableObject/UnitType/UnitType.cs
+++ Heroes of Mighty and Magic and the Storm/Assets/ScriptableObject/UnitType/UnitType.cs
@@ -1,21 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using SubjectNerd.Utilities;
 
 [CreateAssetMenu(menuName = "UnitType")]
 public class UnitType : ScriptableObject
 {
+    public int tab;
+
     public Sprite icon;
     public Race race;
     public RuntimeAnimatorController animControl;
 
+    public int level = 1;
     public int att, def, hp;
     public Vector2Int damage;
     public int speed = 5;
 
     public AttackType attackType;
-    [Reorderable]
     public List<Trait> traits;
 
     [Header("每周产量")]
@@ -26,11 +27,10 @@ public class UnitType : ScriptableObject
     public UnitSize size = UnitSize.middle;
     public ArmorType armorType;
 
-    [Header("远程攻击才需要")]
     public int ammo = 12;
     public GameObject missile;
     public Vector2 launchPos;
-    [Header("音效")]
+
     public AudioClip sound_attack;
     public AudioClip sound_attackImpact;
     public AudioClip[] sound_walk;
