671378659731f1d95d6f6b07b5d03ce7be0a5b81
Flzzyf <2981019122@qq.com>
2018-11-06 10:18:46 +0800

    主界面按钮高亮

---
+++ Heroes of Mighty and Magic and the Storm/Assets/Scripts/MainMenu/Editor/MainMenuButtonEditor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MainMenuButton))]
+public class MainMenuButtonEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MainMenuButton main = (MainMenuButton)target;
+
+        base.OnInspectorGUI();
+
+        //Editor修改过的东西要SetDirty，不然不会保存
+
+        if (main.textComponent.text != main.text)
+        {
+            main.textComponent.text = main.text;
+            EditorUtility.SetDirty(main.textComponent);
+        }
+
+        if (main.bgImage.sprite != main.bg)
+        {
+            main.bgImage.sprite = main.bg;
+            EditorUtility.SetDirty(main.bgImage);
+        }
+
+        if (main.bgOutline.sprite != main.bg)
+        {
+            main.bgOutline.sprite = main.bg;
+            EditorUtility.SetDirty(main.bgOutline);
+        }
+
+        if (main.bgOutline.rectTransform.localScale != main.bgImage.rectTransform.localScale)
+            main.bgOutline.rectTransform.localScale = main.bgImage.rectTransform.localScale;
+
+        if (main.bgOutline.rectTransform.position != main.bgImage.rectTransform.position)
+            main.bgOutline.rectTransform.position = main.bgImage.rectTransform.position;
+
+        EditorUtility.SetDirty(main.bgOutline.rectTransform);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Heroes of Mighty and Magic and the Storm/Assets/Scripts/MainMenu/MainMenuButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Sprite bg;
+    public string text;
+
+    public Image bgImage;
+    public Image bgOutline;
+    public Text textComponent;
+    public Outline textOutline;
+
+    void Start()
+    {
+        Highlight(false);
+    }
+
+    public void Highlight(bool _hightlight)
+    {
+        bgOutline.gameObject.SetActive(_hightlight);
+        textOutline.enabled = _hightlight;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Highlight(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Highlight(false);
+    }
+}
