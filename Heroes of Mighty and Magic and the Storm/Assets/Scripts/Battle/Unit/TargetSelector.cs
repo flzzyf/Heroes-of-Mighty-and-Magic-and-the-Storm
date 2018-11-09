9e3178c3f2b4b22f942a61df47b22bc2f61c9e26
flzzyf <2981019122@qq.com>
2018-11-09 17:59:24 +0800

    加入目标选择器

---
+++ Heroes of Mighty and Magic and the Storm/Assets/Scripts/Battle/Unit/TargetSelector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSelector
+{
+    public void SelectTarget(int _player, TargetFilter _filter)
+    {
+        List<NodeItem> targetNodes = new List<NodeItem>();
+        if (_filter.targetType == TargetType.Unit)
+        {
+            if (_filter.allyOrEnemy == AllyOrEnemy.All)
+            {
+                foreach (Unit item in BattleManager.instance.allUnits)
+                {
+                    targetNodes.Add(item.nodeItem);
+                }
+            }
+            else
+            {
+                //目标为玩家单位
+                int side = (BattleManager.playerSide[_player] + (int)_filter.allyOrEnemy) % 2;
+
+                foreach (Unit item in BattleManager.instance.units[side])
+                {
+                    item.nodeItem.GetComponent<NodeItem_Battle>().ChangeNodeType(BattleNodeType.spellable);
+                }
+            }
+        }
+    }
+}
+
+public enum AllyOrEnemy { All, Ally = 0, Enemy = 1 }
+public enum TargetType { Null, Unit, Node }
+public class TargetFilter
+{
+    public AllyOrEnemy allyOrEnemy;
+    public TargetType targetType;
+}
