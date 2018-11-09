12ca7e34be8f701634d0db4cdef5c4fe5fe1e78f
Flzzyf <2981019122@qq.com>
2018-11-09 16:07:25 +0800

    野怪单位

---
+++ Heroes of Mighty and Magic and the Storm/Assets/Resources/ScriptableObject/AdvantureObject/Obj_Units.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AdvantureObject/Units")]
+public class Obj_Units : AdvantureObject
+{
+    public PocketUnit unit;
+
+    public override void OnInteracted(Hero _hero)
+    {
+        base.OnInteracted(_hero);
+
+        Debug.Log("进入战斗");
+        
+    }
+}
