68f4d6f51e3f65c57f1689b06a43f2d9f03abe43
flzzyf <2981019122@qq.com>
2018-11-24 16:30:18 +0800

    英雄UI

---
+++ Heroes of Mighty and Magic and the Storm/Assets/Scripts/Travel/UI/Panel_HeroItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Panel_HeroItem : MonoBehaviour, IPointerClickHandler
+{
+    public Image image_portrait;
+    public GameObject panel_heroInfo;
+    public GameObject border_highlight;
+
+    public static Panel_HeroItem highlightedHeroItem;
+
+    //鼠标点击
+    public void OnPointerClick(PointerEventData data)
+    {
+        //如果没高亮就高亮，已经高亮则打开英雄UI
+        if (highlightedHeroItem != this)
+        {
+            //解除之前高亮的英雄UI的高亮
+            highlightedHeroItem.border_highlight.SetActive(false);
+            border_highlight.SetActive(true);
+
+            highlightedHeroItem = this;
+        }
+        else
+        {
+            panel_heroInfo.SetActive(true);
+        }
+    }
+}
