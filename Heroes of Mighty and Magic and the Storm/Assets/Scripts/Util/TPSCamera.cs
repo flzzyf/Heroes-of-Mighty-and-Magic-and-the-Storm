81e0a3fbb8af3498f5b4d213459af36266e3d853
flzzyf <2981019122@qq.com>
2018-12-15 15:52:33 +0800

    TPS镜头边缘移动限制

--- Heroes of Mighty and Magic and the Storm/Assets/Scripts/Util/TPSCamera.cs
+++ Heroes of Mighty and Magic and the Storm/Assets/Scripts/Util/TPSCamera.cs
@@ -2,49 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TPSCamera : MonoBehaviour 
+public class TPSCamera : MonoBehaviour
 {
-	public Transform cam;
+    public Transform cam;
 
-	public float panSpeed = 20f;
-	public float panBorderThickness = 10f;
-	public Vector2 panLimit;
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
 
-	public enum Axis { xy, xz};
-	public Axis axis;
+    public enum Axis { xy, xz };
+    public Axis axis;
 
-	void Update()
-	{
-		Vector3 pos = Vector3.zero;
+    void Update()
+    {
+        //鼠标在窗口内，否则结束
+        if (Input.mousePosition.x < 0 || Input.mousePosition.x > Screen.width ||
+            Input.mousePosition.y < 0 || Input.mousePosition.y > Screen.height)
+        {
+            return;
+        }
 
-		if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
-		{
-			if (axis == Axis.xy)
-				pos.y = 1;
-			else
-				pos.z = 1;
-		}
-		if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
-		{
-			if (axis == Axis.xy)
-				pos.y = -1;
-			else
-				pos.z = -1;
-		}
-		if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
-		{
-			pos.x = -1;
-		}
-		else if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
-		{
-			pos.x = 1;
-		}
+        Vector3 dir = GetMovingDir();
 
-		//边缘移动限制
-		//pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
-		//pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
+        //镜头边缘移动限制
+        if (cam.position.x < 0 || cam.position.x > panLimit.x)
+            dir.x = 0;
 
-		cam.Translate(pos * panSpeed * Time.deltaTime, Space.World);
+        if (axis == Axis.xy)
+        {
+            if ((cam.position.y < 0 && dir.y < 0) ||
+             (cam.position.y > panLimit.y && dir.y > 0))
+                dir.y = 0;
+        }
+        else
+        {
+            if ((cam.position.z < 0 && dir.y < 0) ||
+                    (cam.position.z > panLimit.y && dir.y > 0))
+                dir.y = 0;
+        }
 
-	}
+        //如果是xz轴移动
+        if (axis == Axis.xz)
+        {
+            dir.z = dir.y;
+            dir.y = 0;
+        }
+
+        cam.Translate(dir * panSpeed * Time.deltaTime, Space.World);
+    }
+
+    //获取镜头移动方向
+    Vector2 GetMovingDir()
+    {
+        Vector2 pos = Vector2.zero;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.y = 1;
+        }
+        else if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.y = -1;
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x = -1;
+        }
+        else if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x = 1;
+        }
+
+        return pos;
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector3 mapBorder = new Vector3(panLimit.x, 0, panLimit.y);
+        Gizmos.DrawWireCube(mapBorder / 2, mapBorder);
+    }
 }
