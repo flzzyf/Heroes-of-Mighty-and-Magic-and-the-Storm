ab2b6ac39db87b192feba5727b39558be56fdedb
flzzyf <2981019122@qq.com>
2018-12-15 17:51:38 +0800

    一键分兵

--- Heroes of Mighty and Magic and the Storm/Assets/Scripts/Travel/UI/HeroUI_PocketUnit.cs
+++ Heroes of Mighty and Magic and the Storm/Assets/Scripts/Travel/UI/HeroUI_PocketUnit.cs
@@ -6,95 +6,99 @@ using UnityEngine.UI;
 
 public class HeroUI_PocketUnit : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
 {
-	public Image portrait;
-	public Text text_num;
+    public Image portrait;
+    public Text text_num;
 
-	public GameObject border_selected;
+    public GameObject border_selected;
 
-	PocketUnit unit;
+    public PocketUnit unit;
 
-	static HeroUI_PocketUnit selectedPanel;
+    public static HeroUI_PocketUnit selectedPanel;
 
-	void Start()
-	{
-		border_selected.SetActive(false);
-	}
+    void Start()
+    {
+        border_selected.SetActive(false);
+    }
 
-	//设置
-	public void Set(PocketUnit _unit)
-	{
-		unit = _unit;
+    //设置
+    public void Set(PocketUnit _unit)
+    {
+        unit = _unit;
 
-		if (!portrait.enabled)
-			portrait.enabled = true;
-		portrait.sprite = _unit.type.icon;
-		text_num.text = _unit.num + "";
-	}
-	//清空
-	public void Clear()
-	{
-		portrait.enabled = false;
-		text_num.text = "";
+        if (!portrait.enabled)
+            portrait.enabled = true;
+        portrait.sprite = _unit.type.icon;
+        text_num.text = _unit.num + "";
+    }
+    public void Refresh()
+    {
+        text_num.text = unit.num + "";
+    }
+    //清空
+    public void Clear()
+    {
+        portrait.enabled = false;
+        text_num.text = "";
 
-		unit = null;
-	}
+        unit = null;
+    }
 
-	//选中
-	public void Select()
-	{
-		border_selected.SetActive(true);
+    //选中
+    public void Select()
+    {
+        border_selected.SetActive(true);
 
-		selectedPanel = this;
-	}
-	public void Deselect()
-	{
-		border_selected.SetActive(false);
-		selectedPanel = null;
-	}
+        selectedPanel = this;
+    }
+    public void Deselect()
+    {
+        border_selected.SetActive(false);
+        selectedPanel = null;
+    }
 
-	//鼠标进入
-	public void OnPointerEnter(PointerEventData eventData)
-	{
-		if (unit != null)
-			Panel_HeroUI.instance.text_bottomInfo.SetText(LocalizationMgr.instance.GetText("Select") + unit.type.unitName);
-	}
-	//鼠标点击
-	public void OnPointerClick(PointerEventData eventData)
-	{
-		//未选中物体
-		if(selectedPanel == null)
-		{
-			//且不为空，则选中
-			if(unit != null)
-				Select();
-		}
-		else
-		{
-			//已经选中物体。为空则移动。不为空：不同单位则交换，同种单位叠加
-			if (unit == null)
-			{
-				Set(selectedPanel.unit);
-				selectedPanel.Clear();
-			}
-			else
-			{
-				if(unit.type != selectedPanel.unit.type)
-				{
-					//交换
-					PocketUnit temp = unit;
-					Set(selectedPanel.unit);
-					selectedPanel.Set(temp);
-				}
-				else
-				{
-					//叠加
-					unit.num += selectedPanel.unit.num;
-					Set(unit);
-					selectedPanel.Clear();
-				}
-			}
+    //鼠标进入
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (unit != null)
+            Panel_HeroUI.instance.text_bottomInfo.SetText(LocalizationMgr.instance.GetText("Select") + unit.type.unitName);
+    }
+    //鼠标点击
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //未选中物体
+        if (selectedPanel == null)
+        {
+            //且不为空，则选中
+            if (unit != null)
+                Select();
+        }
+        else
+        {
+            //已经选中物体。为空则移动。不为空：不同单位则交换，同种单位叠加
+            if (unit == null)
+            {
+                Set(selectedPanel.unit);
+                selectedPanel.Clear();
+            }
+            else
+            {
+                if (unit.type != selectedPanel.unit.type)
+                {
+                    //交换
+                    PocketUnit temp = unit;
+                    Set(selectedPanel.unit);
+                    selectedPanel.Set(temp);
+                }
+                else
+                {
+                    //叠加
+                    unit.num += selectedPanel.unit.num;
+                    Set(unit);
+                    selectedPanel.Clear();
+                }
+            }
 
-			selectedPanel.Deselect();
-		}
-	}
+            selectedPanel.Deselect();
+        }
+    }
 }
--- Heroes of Mighty and Magic and the Storm/Assets/Scripts/Travel/UI/Panel_HeroUI.cs
+++ Heroes of Mighty and Magic and the Storm/Assets/Scripts/Travel/UI/Panel_HeroUI.cs
@@ -5,84 +5,104 @@ using UnityEngine.UI;
 
 public class Panel_HeroUI : Singleton<Panel_HeroUI>
 {
-	public Image portrait;
-	public Image portraitBG;
-	public Text heroName;
+    public Image portrait;
+    public Image portraitBG;
+    public Text heroName;
 
-	public Panel_MoraleAndLuck moraleAndLuck;
+    public Panel_MoraleAndLuck moraleAndLuck;
 
-	public GameObject ui;
+    public GameObject ui;
 
-	public HeroUI_PocketUnit[] pocketUnits;
+    public HeroUI_PocketUnit[] pocketUnits;
 
-	//英雄属性：攻防、法力、知识
-	public Text[] text_stats;
+    //英雄属性：攻防、法力、知识
+    public Text[] text_stats;
 
-	public LocalizationText text_bottomInfo;
+    public LocalizationText text_bottomInfo;
 
-	public HeroUI_Skill panel_exp;
-	public HeroUI_Skill panel_mana;
+    public HeroUI_Skill panel_exp;
+    public HeroUI_Skill panel_mana;
 
-	public HeroUI_Skill[] skills;
+    public HeroUI_Skill[] skills;
 
-	void Start()
-	{
-		ui.SetActive(false);
-	}
+    void Start()
+    {
+        ui.SetActive(false);
+    }
 
-	//设置界面英雄
-	public void Set(Hero _hero)
-	{
-		//英雄头像
-		portrait.sprite = _hero.heroType.icon;
-		portraitBG.sprite = _hero.heroType.race.sprite_bg;
+    //设置界面英雄
+    public void Set(Hero _hero)
+    {
+        //英雄头像
+        portrait.sprite = _hero.heroType.icon;
+        portraitBG.sprite = _hero.heroType.race.sprite_bg;
 
-		heroName.text = _hero.heroType.heroName;
+        heroName.text = _hero.heroType.heroName;
 
-		//士气和运气
-		moraleAndLuck.SetMorale(_hero.morale);
-		moraleAndLuck.SetLuck(_hero.luck);
+        //士气和运气
+        moraleAndLuck.SetMorale(_hero.morale);
+        moraleAndLuck.SetLuck(_hero.luck);
 
-		//更新单位信息
-		for (int i = 0; i < _hero.pocketUnits.Count; i++)
-		{
-			pocketUnits[i].Set(_hero.pocketUnits[i]);
-		}
-		//重置多余格子
-		for (int i = _hero.pocketUnits.Count; i < 7; i++)
-		{
-			pocketUnits[i].Clear();
-		}
+        //更新单位信息
+        for (int i = 0; i < _hero.pocketUnits.Count; i++)
+        {
+            pocketUnits[i].Set(_hero.pocketUnits[i]);
+        }
+        //重置多余格子
+        for (int i = _hero.pocketUnits.Count; i < 7; i++)
+        {
+            pocketUnits[i].Clear();
+        }
 
-		//设置英雄属性
-		text_stats[0].text = _hero.att + "";
-		text_stats[1].text = _hero.def + "";
-		text_stats[2].text = _hero.power + "";
-		text_stats[3].text = _hero.knowledge + "";
+        //设置英雄属性
+        text_stats[0].text = _hero.att + "";
+        text_stats[1].text = _hero.def + "";
+        text_stats[2].text = _hero.power + "";
+        text_stats[3].text = _hero.knowledge + "";
 
-		//英雄技能栏
-		for (int i = 0; i < _hero.skills.Count; i++)
-		{
-			skills[i].Set(_hero.skills[i]);
-		}
-		for (int i = _hero.skills.Count; i < skills.Length; i++)
-		{
-			skills[i].Clear();
-		}
+        //英雄技能栏
+        for (int i = 0; i < _hero.skills.Count; i++)
+        {
+            skills[i].Set(_hero.skills[i]);
+        }
+        for (int i = _hero.skills.Count; i < skills.Length; i++)
+        {
+            skills[i].Clear();
+        }
 
-		//经验值和魔法
-		panel_mana.text_name.SetText(_hero.mana + "/" + _hero.mana_max);
-	}
+        //经验值和魔法
+        panel_mana.text_name.SetText(_hero.mana + "/" + _hero.mana_max);
+    }
 
-	public void Enter(Hero _hero)
-	{
-		Set(_hero);
+    public void Enter(Hero _hero)
+    {
+        Set(_hero);
 
-		ui.SetActive(true);
-	}
+        ui.SetActive(true);
+    }
 
-	public void Quit()
-	{
-		ui.SetActive(false);
-	}
+    public void Quit()
+    {
+        ui.SetActive(false);
+    }
+
+    //一键分兵
+    public void SmartSplit()
+    {
+        if (HeroUI_PocketUnit.selectedPanel != null)
+        {
+            for (int i = 0; i < pocketUnits.Length && HeroUI_PocketUnit.selectedPanel.unit.num > 1; i++)
+            {
+                //单位栏为空则，选中的单位数量-1，在这一栏位创建1个副本
+                if (pocketUnits[i].unit == null)
+                {
+                    HeroUI_PocketUnit.selectedPanel.unit.num--;
+
+                    PocketUnit unit = new PocketUnit(HeroUI_PocketUnit.selectedPanel.unit.type, 1);
+                    pocketUnits[i].Set(unit);
+                }
+            }
+            HeroUI_PocketUnit.selectedPanel.Refresh();
+        }
+    }
 }
