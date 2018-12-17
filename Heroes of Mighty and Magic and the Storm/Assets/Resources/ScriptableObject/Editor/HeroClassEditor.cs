3117e320d922a29cc15b2ed334c327876958cc59
flzzyf <2981019122@qq.com>
2018-12-17 21:08:30 +0800

    英雄职业

---
+++ Heroes of Mighty and Magic and the Storm/Assets/Resources/ScriptableObject/Editor/HeroClassEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(HeroClass))]
+public class HeroClassEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        HeroClass main = (HeroClass)target;
+
+        //名称 
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Name", LocalizationMgr.instance.GetText(main.name));
+        if (GUILayout.Button("编辑名称"))
+        {
+            LocalizationWindow.ShowWindow(main.name);
+        }
+        EditorGUILayout.EndHorizontal();
+
+        //描述文本
+        EditorGUILayout.LabelField("描述", LocalizationMgr.instance.GetText(main.name + "_Info"));
+        if (GUILayout.Button("编辑"))
+        {
+            LocalizationWindow.ShowWindow(main.name + "_Info");
+        }
+
+        base.OnInspectorGUI();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Heroes of Mighty and Magic and the Storm/Assets/Resources/ScriptableObject/HeroClass/HeroClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "HeroClass")]
+public class HeroClass : ScriptableObject
+{
+    public int att, def, power, knowledge;
+
+    public HeroStatIncreaseRate statRate_before10;
+    public HeroStatIncreaseRate statRate_after10;
+
+    [System.Serializable]
+    public struct HeroStatIncreaseRate
+    {
+        public int att, def, power, knowledge;
+    }
+}
